084e178b9e399311ddd011a76a62e2aa3eddcdf3
Dom-Dermann <dominikdermann@gmail.com>
2019-04-08 18:26:39 +0200

    matplotlib chart with title added

--- AmazonCrawler.py
+++ AmazonCrawler.py
@@ -2,12 +2,13 @@ import requests
 import argparse
 import pandafy
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
 import datetime
 from bs4 import BeautifulSoup
 
 def amazon_single_spider(url):
     price = 'price'
+    title = 'title'
 
     try:
         source_code = requests.get(url)
@@ -16,19 +17,29 @@ def amazon_single_spider(url):
 
         for our_price in soup.findAll('span', {'id': 'priceblock_ourprice'}):
             price = our_price.getText()
+
+        for product_title in soup.findAll('span', {'id': 'productTitle'}):
+            title = product_title.getText()
     except:
         pass
 
-    return price
+    return price, title
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Provide the URL of a product you want monitored.")
     parser.add_argument('URL', type=str, help='provide you URL of your product.')
     args = parser.parse_args()
 
+    # collect URLs to be scanned
+    with open("urls.txt", "w+") as url_file:
+        url_file.write(args.URL + '\n')    
+
     # get crawling result and convert to a float
     result = []
-    result.append(amazon_single_spider(args.URL))
+    price, title = amazon_single_spider(args.URL)
+    title = title.replace("\r", "")
+    title = title.strip()
+    result.append(price)
     float_price_array = pandafy.prices_to_floats(result)
     float_price = float_price_array[0]
 
@@ -48,6 +59,6 @@ if __name__ == "__main__":
         df.to_csv("single-crawler-monitor.csv", index=False)
 
     plot_date = pd.read_csv("single-crawler-monitor.csv", parse_dates=['Time'], index_col='Time')
-    plot_date.plot()
-    matplotlib.pyplot.show()
-    
+    plt.plot(plot_date)
+    plt.title(title + " price curve")
+    plt.show()
